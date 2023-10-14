5100091e009d6f3949e9a4d784425ee71e75b529
SofiaD03 <sofidelgado32@gmail.com>
2023-10-14 12:21:39 -0600

    add: login

--- js/login.js
+++ js/login.js
@@ -3,8 +3,21 @@ function login(){
     let usuario = document.getElementById("usuario").value;
     let contra = document.getElementById("contra").value;
 
-    // Comprueba si las credenciales son válidas (en una aplicación real, esto se haría en el servidor)
-    if (usuario === 'usuario' && contra === 'contraseña') {
+
+    const login = {
+        user: usuario.value,
+        pass: contra.value
+    };
+
+    /* Comprueba si las credenciales son válidas (en una aplicación real, esto se haría en el servidor)
+    if (usuario === login.user && contra === login.pass) {
+        window.location="Menu.html"
+        // En una aplicación real, aquí redirigirías al usuario a la página de inicio.
+    } else {
+        alert("Datos Malos");
+    }*/
+
+    if (usuario === "usuario" && contra === "contraseña") {
         window.location="Menu.html"
         // En una aplicación real, aquí redirigirías al usuario a la página de inicio.
     } else {
