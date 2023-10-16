f72903a7c24111ff27d67c1464307790ea928cc4
SofiaD03 <sofidelgado32@gmail.com>
2023-10-15 18:53:41 -0600

    add_ login

--- js/login.js
+++ js/login.js
@@ -4,19 +4,6 @@ function login(){
     let contra = document.getElementById("contra").value;
 
 
-    
-
-    /* Comprueba si las credenciales son válidas (en una aplicación real, esto se haría en el servidor)
-
-
-    if (usuario === login.user && contra === login.pass) {
-        window.location="Menu.html"
-        // En una aplicación real, aquí redirigirías al usuario a la página de inicio.
-    } else {
-        alert("Datos Malos");
-    }*/
-
-
     async function login(event) {
         event.preventDefault();
     
@@ -52,11 +39,11 @@ function login(){
     }
 
 
-    if (usuario === "usuario" && contra === "contraseña") {
+   /* if (usuario === "usuario" && contra === "contraseña") {
         window.location="Menu.html"
         // En una aplicación real, aquí redirigirías al usuario a la página de inicio.
     } else {
         alert("Datos Malos");
-    }
+    }*/
 
 }
