de1ad13c0e80016886c72050697af85d5c41d898
SofiaD03 <sofidelgado32@gmail.com>
2023-10-15 18:52:07 -0600

    add: login

--- js/login.js
+++ js/login.js
@@ -4,18 +4,53 @@ function login(){
     let contra = document.getElementById("contra").value;
 
 
-    const login = {
-        user: usuario.value,
-        pass: contra.value
-    };
+    
 
     /* Comprueba si las credenciales son válidas (en una aplicación real, esto se haría en el servidor)
+
+
     if (usuario === login.user && contra === login.pass) {
         window.location="Menu.html"
         // En una aplicación real, aquí redirigirías al usuario a la página de inicio.
     } else {
         alert("Datos Malos");
     }*/
+
+
+    async function login(event) {
+        event.preventDefault();
+    
+        const login = {
+            usuario: usuario.value,
+            contraseña: contra.value
+        };
+    
+        let apiUrlEndpoint = `${apiUrl}/Login`;
+    
+        
+        try {
+            const response = await fetch(apiUrlEndpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(login)
+            });
+    
+            const data = await response.json();
+    
+            if (response.status == 'true') {
+                window.location="Menu.html"
+            }
+         else {
+                console.error('Contraseña o Usuario Incorrecto', data.mensaje);
+                alert("Contraseña o Usuario Incorrecto");
+            }
+        } catch (error) {
+            console.error('Contraseña o Usuario Incorrecto', error);
+        }
+    }
+
 
     if (usuario === "usuario" && contra === "contraseña") {
         window.location="Menu.html"
