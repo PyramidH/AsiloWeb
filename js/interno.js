daffe129acf3f364ee35969018482a5dddd96a80
Marvin Alvarado <marvinalvarado2000@outlook.es>
2023-10-23 12:33:16 -0600

    Editar Interno
    Editar finalizado, faltan pequeños detalles para completar la vista

--- js/interno.js
+++ js/interno.js
@@ -21,11 +21,12 @@ async function cargarListaInternos() {
                 <tr>
                     <th>ID</th>
                     <th>Nombre</th>
-                    <th>F. Nacimiento</th>
+                    <th>Apellido</th>
+                    <th>F. Nacim.</th>
                     <th>Gen</th>
                     <th>Telefono</th>
-                    <th>C. Emergencia</th>
-                    <th>Num. Emergencia</th>
+                    <th>C. Emer.</th>
+                    <th>Num. Emer.</th>
                     <th>Email</th>
                     <th>Opciones</th>
                 </tr>
@@ -35,11 +36,13 @@ async function cargarListaInternos() {
             const tableBody = document.createElement('tbody');
             data.response.forEach(internos => {
                 const row = document.createElement('tr');
+                row.setAttribute('data-id', internos.idInterno);
                 const fechaNacimiento = new Date(internos.fechaNacimiento);
                 const fechaSolo = fechaNacimiento.toISOString().split('T')[0];
                 row.innerHTML = `
                     <td>${internos.idInterno}</td>
-                    <td>${internos.nombre + " " + internos.apellido}</td>
+                    <td>${internos.nombre}</td>
+                    <td>${internos.apellido}</td>
                     <td>${fechaSolo}</td>
                     <td>${internos.genero}</td>
                     <td>${internos.telefono}</td>
@@ -47,7 +50,7 @@ async function cargarListaInternos() {
                     <td>${internos.numContactoEmergencia}</td>
                     <td>${internos.email}</td>
                     <td>
-                    <button class="btn btn-primary" onclick="editarInterno (${internos.idInterno})">Editar</button>
+                    <button class="btn btn-primary" onclick="editarInterno(${internos.idInterno})">Editar</button>
                     <button class="btn btn-cancel" onclick="eliminarInterno(${internos.idInterno})">Eliminar</button>
                     </td>
                 `;
@@ -135,6 +138,69 @@ function limpiarFormulario() {
     document.getElementById('email').value = '';
     document.getElementById('genero').checked = false;
     document.getElementById('fecha').value = '';
+}
+
+function editarInterno(id) {
+    const fila = document.querySelector(`tr[data-id="${id}"]`);
+    const celdas = fila.querySelectorAll('td');
+    celdas.forEach(function (celda, index) {
+        if (index < celdas.length - 1) {
+            const valorOriginal = celda.innerText;
+            celda.innerHTML = `<input type="text" style='width:100%' value="${valorOriginal}">`;
+        }
+    });
+
+    const btnEditar = fila.querySelector('button.btn-primary');
+    btnEditar.textContent = 'Guardar';
+    btnEditar.classList.remove("btn-primary");
+    btnEditar.classList.add("btn-save");
+    btnEditar.onclick = function () {
+        actualizar(id);
+    };
+    const btnEliminar = fila.querySelector('button.btn-cancel');
+    btnEliminar.textContent = 'Cancelar';
+    btnEliminar.onclick = function () {
+        cargarListaInternos();
+    };
+}
+
+async function actualizar(id) {
+    event.preventDefault();
+    const fila = document.querySelector(`tr[data-id="${id}"]`);
+    const celdas = fila.querySelectorAll('td');
+    const interno = {
+        idInterno: celdas[0].querySelector('input').value,
+        nombre: celdas[1].querySelector('input').value,
+        apellido: celdas[2].querySelector('input').value,
+        fechaNacimiento: celdas[3].querySelector('input').value,
+        genero: celdas[4].querySelector('input').value,
+        telefono: celdas[5].querySelector('input').value,
+        contactoEmergencia: celdas[6].querySelector('input').value,
+        numContactoEmergencia: celdas[7].querySelector('input').value,
+        email: celdas[8].querySelector('input').value
+    };
+
+    let apiUrlEndpoint = `${apiUrl}/Editar`;
+
+    try {
+        const response = await fetch(apiUrlEndpoint, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(interno)
+        });
+
+        const data = await response.json();
+
+        if (response.status === 200) {
+            cargarListaInternos();
+        } else {
+            console.error('Error en la respuesta de la API:', data.mensaje);
+        }
+    } catch (error) {
+        console.error('Error al actualizar el Interno:', error);
+    }
 }
 
 document.getElementById('registrarInterno').addEventListener('submit', guardarInterno);
