43c59861056fc2b50b5d8dfd90d9187599dc00ed
Marvin Alvarado <marvinalvarado2000@outlook.es>
2023-10-25 00:29:57 -0600

    Logica de sesiones abierta completa
    Se cambio el metodo de cookies a localStorage

--- js/login.js
+++ js/login.js
@@ -9,22 +9,28 @@ async function login(user, pass) {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
-            }
+            },
         });
 
         const data = await response.json();
-        console.log(data);
-        if (data.response == true) {
-            console.log("Sesion Iniciada Correctamente");
-            console.log(document.cookie);
-            var userInfoCookie = getCookie("InformacionSesion");
-            var userInfo = JSON.parse(userInfoCookie);
-            var usuario = userInfo.Usuario;
-            var rol = userInfo.Rol;
-            var sesionActiva = userInfo.SesionActiva;
-            var email = userInfo.email;
-            var nombre = userInfo.nombre;
-            switch (rol) {
+        console.log(data.response);
+        if (data.sesion === true) {
+            var idUsuario = data.response.idUsuario;
+            var nombre = data.response.nombre;
+            var usuario = data.response.usuario1;
+            var contraseña = data.response.contraseña;
+            var rol = data.response.rol;
+            var email = data.response.email;
+            var sesion = data.sesion;
+            localStorage.setItem("idUsuario", idUsuario);
+            localStorage.setItem("nombre", nombre);
+            localStorage.setItem("usuario", usuario);
+            localStorage.setItem("contraseña", contraseña);
+            localStorage.setItem("rol", rol);
+            localStorage.setItem("email", email);
+            localStorage.setItem("sesion", sesion);
+            var rolUsuario = localStorage.getItem("rol");
+            switch (rolUsuario) {
                 case "Laboratorio":
                     window.location = "dashboardLaboratorio.html";
                     break;
@@ -50,17 +56,28 @@ async function login(user, pass) {
     }
 }
 
-function getCookie(cookieName) {
-    const name = cookieName + "=";
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const cookieArray = decodedCookie.split(';');
-    for (let i = 0; i < cookieArray.length; i++) {
-        let cookie = cookieArray[i].trim();
-        if (cookie.indexOf(name) == 0) {
-            return cookie.substring(name.length, cookie.length);
+function isActive() {
+    if (localStorage.getItem("sesion") == "true") {
+        var rolUsuario = localStorage.getItem("rol");
+        switch (rolUsuario) {
+            case "Laboratorio":
+                window.location = "dashboardLaboratorio.html";
+                break;
+            case "Asilo":
+                window.location = "dashboardAsilo.html";
+                break;
+            case "Fundación":
+                window.location = "dashboardFundacion.html"
+                break;
+            case "Médico General":
+                window.location = "dashboardMedicoGeneral.html"
+                break;
+            case "Médico Especialidad":
+                window.location = "dashboardMedicoEspecialista.html"
+                break;
         }
     }
-    return "";
 }
+
 
 
