54cc28d1d81b954d7107d6d1dfed9fe77a99101f
Marvin Alvarado <marvinalvarado2000@outlook.es>
2023-10-31 17:58:44 -0600

    Visita Medica
    Permite Hacer diagnosticos

--- js/visitaMedica.js
+++ js/visitaMedica.js
@@ -1,5 +1,14 @@
 const apiUrlInternos = 'http://malvarado-001-site1.atempurl.com/api/CitaMedica';
+const apiUrlVisita = 'http://malvarado-001-site1.atempurl.com/api/VisitaMedicas';
+
 const pacientes_list = document.getElementById('pacientes-list');
+var txtDiagnostico = document.getElementById('diagnostico');
+var txtMedicamento = document.getElementById('medicamento');
+var txtCantidad = document.getElementById('cantidad');
+var txtTiempo = document.getElementById('tiempo');
+var txtObservaciones = document.getElementById('observaciones');
+var txtId = document.getElementById('id');
+var txtPaciente = document.getElementById('paciente');
 
 async function cargarListaPacientes() {
     try {
@@ -26,7 +35,7 @@ async function cargarListaPacientes() {
                     var medico = cita.idMedicoNavigation.nombre + " " + cita.idMedicoNavigation.apellido;
                 }
                 var medicoLoged = localStorage.getItem("nombre");
-                if (medico == medicoLoged) {
+                if (medico == medicoLoged && cita.estado == 'Asignada') {
                     const row = document.createElement('tr');
                     row.setAttribute('data-id', cita.idCita);
                     var fechaOriginal = cita.fechaHora;
@@ -37,13 +46,14 @@ async function cargarListaPacientes() {
                     var hora = fecha.getHours().toString();
                     var minutos = fecha.getMinutes().toString().padStart(2, '0');
                     var fechaFormateada = dia + "-" + mes + "-" + año + " " + hora + ":" + minutos;
+                    var paciente = cita.idInternoNavigation.nombre + " " + cita.idInternoNavigation.apellido;
                     row.innerHTML = `
                         <td>${cita.idCita}</td>
                         <td>${fechaFormateada}</td>
                         <td>${cita.motivo}</td>
-                        <td>${cita.idInternoNavigation.nombre + " " + cita.idInternoNavigation.apellido}</td>
+                        <td>${paciente}</td>
                         <td>
-                        <button type="button" class="btn btn-primary" onclick="botonFooter(${cita.idCita})">Atender</button>
+                        <button type="button" class="btn btn-primary" onclick="ActivarControles(${cita.idCita + ",'" + paciente + "'"})">Atender</button>
                         <button class="btn btn-cancel" onclick="denegarCita(${cita.idCita})">Denegar</button>
                         </td>
                     `;
@@ -59,3 +69,80 @@ async function cargarListaPacientes() {
         console.error('Error al cargar la lista de citas:', error);
     }
 }
+
+function ActivarControles(id, paciente) {
+    txtDiagnostico.disabled = false;
+    txtMedicamento.disabled = false;
+    txtCantidad.disabled = false;
+    txtTiempo.disabled = false;
+    txtObservaciones.disabled = false;
+    txtId.value = id;
+    txtPaciente.value = paciente;
+}
+function limpiarFormulario(id, paciente) {
+    txtDiagnostico.disabled = true;
+    txtMedicamento.disabled = true;
+    txtCantidad.disabled = true;
+    txtTiempo.disabled = true;
+    txtObservaciones.disabled = true;
+    txtId.value = "";
+    txtPaciente.value = "";
+    txtDiagnostico.value = "";
+    txtMedicamento.value = "";
+    txtCantidad.value = "";
+    txtTiempo.value = "";
+    txtObservaciones.value = "";
+}
+
+async function guardarVisita(event) {
+    event.preventDefault();
+    var cantidad = parseInt(txtCantidad.value);
+    var idCita = parseInt(txtId.value);
+    const visita = {
+        diagnostico: txtDiagnostico.value,
+        medicamento: txtMedicamento.value,
+        cantidad: cantidad,
+        tiempo: txtTiempo.value,
+        observaciones: txtObservaciones.value,
+        idCita: idCita
+    };
+    let apiUrlEndpoint = `${apiUrlVisita}/Guardar`;
+    try {
+        const response = await fetch(apiUrlEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(visita)
+        });
+        const data = await response.json();
+        if (response.status === 200) {
+            limpiarFormulario();
+        } else {
+            console.error('Error en la respuesta de la API:', data.mensaje);
+        }
+    } catch (error) {
+        console.error('Error al guardar el Interno:', error);
+    }
+    //Editar el estado de la Cita
+    const cita = {
+        idCita: idCita,
+        estado: "Atendida"
+    };
+    let apiUrlEndpoint1 = `${apiUrlInternos}/Editar`;
+    try {
+        const response = await fetch(apiUrlEndpoint1, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(cita)
+        });
+        const data = await response.json();
+    } catch (error) {
+        console.error('Error al actualizar la cita:', error);
+    }
+    cargarListaPacientes();
+}
+document.getElementById('registrarVisita').addEventListener('submit', guardarVisita);
+
